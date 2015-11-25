a12e75475088a87bc72618d65fd4cb6ce7e00456
Caspar Rinne <caspar.rinne@gmail.com>
2015-11-25 16:09:38 +0200

    Implemeting caesar shift

--- main.c
+++ main.c
@@ -1,37 +1,51 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <getopt.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define caesar(x) rot(13, x)
+#define decaesar(x) rot(13, x)
+#define decrypt_rot(x, y) rot((26-x), y)
+
+void remove_newline_ch(char *line);
+void rot(int c, char *str);
+
+int main()
+{
+	char str[80] = "";
+
+  printf("Enter sentence: ");
+  fgets(str, sizeof(str), stdin);
+  remove_newline_ch(str);
 
 
-int main(int argc, char *argv[]) {
+	printf("Original: %s\n", str);
+	caesar(str);
+	printf("Encrypted: %s\n", str);
+	decaesar(str);
+	printf("Decrypted: %s\n", str);
 
-    char *word = "";
-    int key = 0;
-    int count = 0;
-    char ch;
+	return 0;
+}
 
-    while ((ch = getopt(argc, argv,"k:w:")) != EOF)
-        switch (ch) {
+void rot(int c, char *str)
+{
+	int l = strlen(str);
+	const char *alpha[2] = { "abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
 
-            case 'k':
-                key = atoi(optarg);
-                break;
-            case 'w':
-                word = optarg;
-                break;
-        
-            default: 
-                fprintf(stderr, "Unknown option '%s'\n", optarg);
-                return 1;
-        }
-        
-        printf("key is here %d\n", key);
-        printf("word is here %s\n", word);
+	int i;
+	for (i = 0; i < l; i++)
+	{
+		if (!isalpha(str[i]))
+			continue;
 
-        argc -= optind;
-        argv += optind;
+		str[i] = alpha[isupper(str[i])][((int)(tolower(str[i])-'a')+c)%26];
+	}
+}
 
-        return 0;
-
+void remove_newline_ch(char *line)
+{
+    int new_line = strlen(line) -1;
+    if (line[new_line] == '\n')
+        line[new_line] = '\0';
 }
